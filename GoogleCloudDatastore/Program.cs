947ea582b9ed24e2b8964f70ff7a13c02b71f504
Andy Macdonald <amacdonald@sidebytes.com>
2017-10-21 22:27:11 +0100

    Added a menu to move allow back to back writes or queries

--- GoogleCloudDatastore/Program.cs
+++ GoogleCloudDatastore/Program.cs
@@ -10,8 +10,39 @@ namespace GoogleCloudDatastore
     {
         static void Main(string[] args)
         {
-            var project = new Project() { Id = Guid.NewGuid(), Name = "Project One", Owner="Mr Smith",};
-            project.Client = new Client { Name="The Client Name", AddressOne="12345 Some Street",AddressTwo="Someville"};
+            while (true)
+            {
+                Console.WriteLine("1. Write to cloud datastore");
+                Console.WriteLine("2. Query cloud datastore");
+                Console.WriteLine("3. Exit");
+
+                Console.WriteLine();
+                Console.Write("Select an option: ");
+
+                var result = Console.ReadLine();
+
+                switch (Convert.ToInt32(result))
+                {
+                    case 1:
+                        WriteToGoogleCloudDatastore();
+                        break;
+                    case 2:
+                        QueryGoogleCloudDatastore();
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+        
+        private static void WriteToGoogleCloudDatastore()
+        {
+            var project = new Project() { Id = Guid.NewGuid(), Name = "Project One", Owner = "Mr Smith", };
+            project.Client = new Client { Name = "The Client Name", AddressOne = "12345 Some Street", AddressTwo = "Someville" };
 
             project.Tasks = new List<Task>()
             {
@@ -19,22 +50,30 @@ namespace GoogleCloudDatastore
                 new Task(){ Name="Task number two", Description="This is task number two"}
             };
 
-            var projectId = "ENTER PROJECT NAME HERE";
-            var namespaceId = "ENTER NAMESAPCE HERE";
+            //var projectId = "ENTER PROJECT NAME HERE";
+            //var namespaceId = "ENTER NAMESAPCE HERE";
 
-            DatastoreDb db = DatastoreDb.Create(projectId,namespaceId);
-            
+            var projectId = "timeme-dev";
+            var namespaceId = "project";
+
+
+            DatastoreDb db = DatastoreDb.Create(projectId, namespaceId);
+
             var task = project.ToEntity(db);
 
             using (DatastoreTransaction transaction = db.BeginTransaction())
             {
                 transaction.Upsert(task);
                 transaction.Commit();
-
-                Console.WriteLine($"Saved {task.Key.Path[0].Name}");
             }
 
-            Console.ReadLine();
+            Console.WriteLine($"Success! Saved project with ID {task.Key.Path[0].Name}");
+            Console.WriteLine();
+        }
+
+        private static void QueryGoogleCloudDatastore()
+        {
+
         }
     }
 }
